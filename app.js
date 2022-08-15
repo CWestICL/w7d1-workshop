bbb7be07f6a453da2e276a8230ed82455416ef0a
CWestICL <cwest@ic.ac.uk>
2022-08-15 14:21:18 +0100

    Init commit

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 const {buildDB} = require('./db/populateDataBase')
-buildDB()
+buildDB();
 
+const express = require("express");
+const app = express();
+const {Cheese,Board} = require("./models") 
+const path = require("path");
+const port = 3001;
+
+app.get("/", (req,res)=> {
+    res.sendStatus(200);
+});
+
+app.get("/camembert", async (req,res)=> {
+    //res.send("Gathering Camembert data");
+    const cheese = await Cheese.findOne({where: {title: "Camembert"}});
+    let {title, description} = cheese;
+    let payload = {
+        title: title,
+        description: description
+    }
+    res.send(payload);
+});
+
+app.get("/c-cheese", async (req,res)=> {
+    //res.send("Gathering Camembert data");
+    const cheeses = await Cheese.findAll();
+    let cCheeses = cheeses.filter((cheese) => {
+        if (cheese.title[0] === "C") {
+            return true;
+        }
+    })
+    res.send(cCheeses);
+});
+
+app.listen(port, () => {
+    console.log("The server is live and listening at http://localhost:3001");
+});
 
 
